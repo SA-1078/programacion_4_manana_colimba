ab18114f77836eacba8d629dd7e126e470b02534
unknown <santiagocolimba10@gmail.com>
2026-04-08 11:07:58 -0500

    first commit

---
+++ Modulo_1/basics/03_tipos_numericos.kt
@@ -0,0 +1,34 @@
+fun main() {
+    //Tipos Datos
+    //Nunmeros enteros
+    val numero1: Byte=127
+    println("Numero Byte $numero1")
+    val numero2: Short=32_765
+    println("Numero Short $numero2")
+    val numero3: Int=12_122_122
+    println("Numero Int $numero3")
+    val numero4: Long=12_122_122_122_123
+    println("Numero Long $numero4")
+    
+    println("Numeros decimales")
+    val numero5: Float=3.14f
+    println("Numero Float $numero5")
+    
+    val numero6: Double=3.14159265
+    println("Numero Double $numero6")
+    
+    //Inferido
+    val nombre="Juana"
+    val edad=56
+    
+    println("Nombre: $nombre")
+    val nombreTipo=nombre::class.simpleName
+    println("Tipo inferido nombre: ${nombreTipo}")
+    println("Tipo inferido nombre: ${nombre::class.simpleName}")
+    println("Edad: $edad")
+    
+    val edadTipo=edad::class.simpleName
+    println("Tipo inferido edad: ${edadTipo}")
+    println("Tipo inferido edad: ${edad::class.simpleName}")
+
+}
---
+++ Modulo_1/basics/05_readline.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+    //Readline 
+    println("Escribe tu nombre:")
+    val nombre = readLine()
+    println("Nombre Incluido : $nombre")
+    
+    println("Escribe apellido:")
+    val apellido = readLine() ?: "anonimo"
+    println("Apellido Incluido : $apellido")
+
+
+    
+    /*
+    Comentarios Multilinea
+    */
+
+    // Comentarios de una sola linea
+
+    /**
+    * Multilinea
+    * Comentarios
+    * @param nombre del usuario
+    * @return un saludo personalizado
+    */
+}
