059e034ed2b8aacbf23d75a6f385931140ccb335
unknown <santiagocolimba10@gmail.com>
2026-05-04 11:55:18 -0500

    Clase Lunes 04 Mayo 2026, proyecto nuevo de android studio

---
+++ Modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -0,0 +1,28 @@
+// MainActivity.kt
+package com.ute.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.ute.compose.ui.S01_SaludoScreen
+import com.ute.compose.ui.screens.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                // ◀ CAMBIA AQUÍ para probar cada paso:
+                // S01_SaludoScreen()
+                S02_TextScreen()
+                // Paso01_TextFieldScreen()
+                // Paso02_CardScreen()
+                // Paso03_LazyColumnScreen()
+                // Paso04_ScaffoldScreen()
+                // Paso05_NavBarScreen()
+                // Paso06_DialogosScreen()   // ← paso activo
+            }
+        }
+    }
+}
